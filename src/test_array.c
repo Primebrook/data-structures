b8a45da146745fccddbf47f56c91cc6baf535797
KonklaveTtv <141496287+KonklaveTtv@users.noreply.github.com>
2023-08-05 16:58:52 +0200

    Update unittests

--- src/test_array.c
+++ src/test_array.c
@@ -1,12 +1,43 @@
-#include "test_array.h"
+#include <check.h>
 #include "array.h"
-#include <assert.h>
-#include <stdio.h>
 
-void test_initialize() {
-    DynamicArray *dy_arr = initialize(10);
-    assert(dy_arr->size == 0);
-    assert(dy_arr->capacity == 10);
-    printf("test_initialize \033[0;32mPASSED.\033[0m\n");
-};
-void test_insert(){};
+// This is a test setup function that will be called before each test
+void setup(void) {}
+
+// This is a test teardown function that will be called after each test
+void teardown(void) {}
+
+// Sample test case
+START_TEST(test_dynamic_array_creation) {
+    DynamicArray *arr = initialize(INITIAL_CAPACITY, INT);
+    ck_assert_ptr_nonnull(arr);
+    ck_assert_int_eq(size(arr), 0);
+    ck_assert_int_eq(arr->capacity, INITIAL_CAPACITY);
+    ck_assert_int_eq(arr->type, INT);
+    destroy(arr);
+}
+END_TEST
+
+// Add more test cases as needed
+
+int main(void) {
+    Suite *s;
+    SRunner *runner;
+    int number_failed;
+
+    s = suite_create("DynamicArray");
+    TCase *tc_core = tcase_create("Core");
+    
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+    tcase_add_test(tc_core, test_dynamic_array_creation);
+    // Add more test cases to the core test case
+
+    suite_add_tcase(s, tc_core);
+
+    runner = srunner_create(s);
+    srunner_run_all(runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
