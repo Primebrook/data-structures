735c80fe29a5f8b28d836cd7e601f114d71e69a7
KonklaveTtv <141496287+KonklaveTtv@users.noreply.github.com>
2023-08-05 18:19:16 +0200

    Update test_array.h

--- include/test_array.h
+++ include/test_array.h
@@ -1,7 +1,11 @@
 #ifndef TEST_ARRAY_H
 #define TEST_ARRAY_H
 
-void test_initialize();
-void test_insert();
+#include <check.h>
+#include "array.h"
+
+// Declare your test case functions here
+START_TEST(test_dynamic_array_creation);
+// Add more test case declarations as needed
 
 #endif // TEST_ARRAY_H
