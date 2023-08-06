65b0322f4941a2ee3f17a953e6ad08d3418e67a6
Brook <71849503+Primebrook@users.noreply.github.com>
2023-08-06 17:45:29 +0100

    get_at (#10)

--- include/array.h
+++ include/array.h
@@ -9,4 +9,3 @@ typedef struct {
 DynamicArray initialize(size_t initial_capacity, size_t initial_size);
 void set_at(DynamicArray *dy_arr, size_t pos, int value);
 int get_at(DynamicArray *dy_arr, size_t pos);
-
--- src/array.c
+++ src/array.c
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 
 DynamicArray initialize(size_t initial_capacity, size_t initial_size) {
-	if (initial_size > initial_capacity){
-		fprintf(stderr, "Initial size cannot be greater than initial capacity.\n");
-		exit(EXIT_FAILURE);
-	}
+    if (initial_size > initial_capacity) {
+        fprintf(stderr,
+                "Initial size cannot be greater than initial capacity.\n");
+        exit(EXIT_FAILURE);
+    }
     DynamicArray dy_arr;
 
     dy_arr.data = calloc(initial_size, sizeof(int));
@@ -27,37 +28,16 @@ void set_at(DynamicArray *dy_arr, size_t pos, int value) {
     assert(dy_arr->data);
 
     if (pos >= dy_arr->size) {
-        fprintf(stderr, "Specified index is greater than size. \n");
+        fprintf(stderr, "Specified index is greater than size.\n");
         return;
     };
     dy_arr->data[pos] = value;
 };
 
-int get_at(DynamicArray *dy_arr, size_t pos){
-	return 0;	
-}; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int get_at(DynamicArray *dy_arr, size_t pos) {
+    if (pos >= dy_arr->size) {
+        fprintf(stderr, "Specified index is greater than size.\n");
+        exit(EXIT_FAILURE);
+    };
+    return dy_arr->data[pos];
+};
