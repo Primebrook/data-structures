45ee24bf4b25614b2493977ee691604184630620
Brook <71849503+Primebrook@users.noreply.github.com>
2023-08-06 18:54:09 +0100

    rename array -> dynamic_array (#12)

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-#include "test_array.h"
+#include "test_dynamic_array.h"
 #include <stdio.h>
 
 int main() {
-    test_array();
+    test_dynamic_array();
     printf("\n\nAll tests \033[0;32mPASSED.\033[0m\n");
     return 0;
 }
