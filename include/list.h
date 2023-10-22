4b0b392131be9d011a3ed0068a6b49cd7087f3da
Brook <71849503+Primebrook@users.noreply.github.com>
2023-10-22 17:12:39 +0100

    set_at baby (#26)

--- include/list.h
+++ include/list.h
@@ -10,6 +10,7 @@ struct Node {
 Node *list_initialize();
 Node *node_initialize(int value);
 void list_push(Node *list, int value);
-void list_insert(Node *list, size_t post, int value);
+void list_insert(Node *list, size_t pos, int value);
+void list_set_at(Node *list, size_t pos, int value);
 size_t list_length(Node *list);
 void list_free(Node *list);
