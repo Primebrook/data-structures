f98c6c4023d7f2e3ee6a92bfd025dbd0b53491db
Brook <71849503+Primebrook@users.noreply.github.com>
2023-10-23 19:22:45 +0100

    get_at (#27)

--- include/list.h
+++ include/list.h
@@ -12,5 +12,6 @@ Node *node_initialize(int value);
 void list_push(Node *list, int value);
 void list_insert(Node *list, size_t pos, int value);
 void list_set_at(Node *list, size_t pos, int value);
+int list_get_at(Node *list, size_t pos);
 size_t list_length(Node *list);
 void list_free(Node *list);
--- src/test_list.c
+++ src/test_list.c
@@ -34,6 +34,7 @@ void test_list_insert() {
     list_insert(list, 1, 22);
     assert((list->next)->value == 22);
     assert((list->next)->next == NULL);
+    list_free(list);
     puts("test_list_insert \033[0;32mPASSED.\033[0m");
 };
 
@@ -42,6 +43,7 @@ void test_list_set_at_1() {
     list_set_at(list, 0, 22);
     assert(list->value == 22);
     assert(list->next == NULL);
+    list_free(list);
     puts("test_list_set_at_1 \033[0;32mPASSED.\033[0m");
 };
 
@@ -51,7 +53,28 @@ void test_list_set_at_2() {
     list_set_at(list, 1, 465);
     assert((list->next)->value == 465);
     assert((list->next)->next == NULL);
+    list_free(list);
     puts("test_list_set_at_2 \033[0;32mPASSED.\033[0m");
+};
+
+void test_list_get_at_1() {
+    Node *list = list_initialize();
+    int value_0 = list_get_at(list, 0);
+    assert(value_0 == 0);
+    list_free(list);
+    puts("test_list_get_at_1 \033[0;32mPASSED.\033[0m");
+};
+
+void test_list_get_at_2() {
+    Node *list = list_initialize();
+    list_push(list, 124214);
+    list_push(list, 568);
+    int value_1 = list_get_at(list, 1);
+    int value_2 = list_get_at(list, 2);
+    assert(value_1 == 124214);
+    assert(value_2 == 568);
+    list_free(list);
+    puts("test_list_get_at_2 \033[0;32mPASSED.\033[0m");
 };
 
 void test_list_length_1() {
@@ -70,4 +93,6 @@ void test_list() {
     test_list_insert();
     test_list_set_at_1();
     test_list_set_at_2();
+    test_list_get_at_1();
+    test_list_get_at_2();
 };
